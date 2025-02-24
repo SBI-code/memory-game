aa27aa7a4a032ca6b831cbf9d0519212bec42cce
SBI-code <sabreenabari@aol.com>
2025-02-24 22:38:04 +0000

    Feat: Create an array of pairs using .concat to return the exact same array to fill the grid with two copies of the emojis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const emojis = ["🚀", "🪐", "🌍", "🌌", "🔭", "🌕", "☄️", "🌠"];
+
+    // Create an array of pairs
+    let cardsArray = emojis.concat(emojis);
 });
