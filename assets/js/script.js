4ae1f799b0924a03bc1c941f9e9056e202ccad57
SBI-code <sabreenabari@aol.com>
2025-02-24 22:40:21 +0000

    Feat: Randomly sort the cardsArray

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Create an array of pairs
     let cardsArray = emojis.concat(emojis);
+
+    // Shuffle the array items
+    cardsArray.sort(() => Math.random() - 0.5);
 });
